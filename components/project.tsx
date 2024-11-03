a09047eb7b64e92ba47e189e91b4f4fef1f9a043
davidbbe <david123beauchamp@gmail.com>
2024-11-03 12:16:36 +0000

    Favicon and more text updates

--- components/project.tsx
+++ components/project.tsx
@@ -7,7 +7,7 @@ import { motion, useScroll, useTransform } from "framer-motion";
 
 type ProjectProps = (typeof projectsData)[number];
 
-export default function Project({ title, description, tags, imageUrl }: ProjectProps) {
+export default function Project({ title, url, description, tags, imageUrl }: ProjectProps) {
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: ref,
@@ -24,8 +24,9 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
     >
       <section className="bg-gray-100 max-w-4xl border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[24rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[60%] flex flex-col h-full sm:group-even:ml-[18rem]">
-          <h3 className="text-2xl font-semibold">{title}</h3>
-          <p className="mt-2 mb-3 leading-relaxed text-gray-700 dark:text-white/70">{description}</p>
+          <h3 className="mb-1 text-2xl font-semibold">{title}</h3>
+          <p className="mb-2 text-sm leading-relaxed text-gray-900 dark:text-white/70">{url}</p>
+          <p className="mb-2 leading-relaxed text-gray-600 dark:text-white/70">{description}</p>
           <ul className="flex flex-wrap mt-4 gap-1 sm:mt-auto">
             {tags.map((tag, index) => (
               <li
--- lib/data.ts
+++ lib/data.ts
@@ -1,9 +1,10 @@
 import React from "react";
 import { FaReact, FaWordpressSimple, FaShopify } from "react-icons/fa";
 import { SiNextdotjs } from "react-icons/si";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import netflixroulettepic from "@/public/netflix-roulette-website.jpg";
+import uwhmappic from "@/public/UWH-map-website.jpg";
+import particlepic from "@/public/particle-website.jpg";
+import storepic from "@/public/store-website.jpg";
 
 export const links = [
   {
@@ -59,21 +60,31 @@ export const experiencesData = [
 export const projectsData = [
   {
     title: "Underwater Hockey Map",
+    url: "https://uwhmap.com",
     description: "I designed and developed this application. It uses the Google Maps API and Google Places API to view, create, or edit underwater hockey clubs around the world. It has features like login, user accounts, and CRUD functionality for users to dynamically manage the database and pages.",
     tags: ["Next.js", "NextAuth", "Prisma", "PostgreSQL", "Vercel", "Google Maps API", "Google Places API", "Tailwind", "Shadcn", "React Query"],
-    imageUrl: corpcommentImg,
+    imageUrl: uwhmappic,
   },
   {
     title: "Netflix Roulette",
+    url: "https://tvroulette.app",
     description: "I designed and developed this application. It uses the IMDB, TMDB, OMDB, and JustWatch APIs to search for movies and TV shows. It features an interactive UI with multiple animations to retain user attention and engagement. This website has over 1.3k active monthy users and ranks very high on Google for the search term 'Netflix Roulette'.",
     tags: ["Gatsby.js", "GSAP", "Styled Components", "React Query", "Contentful CMS", "Netlify", "IMDB API", "TMDB API", "OMDB API", "JustWatch API"],
-    imageUrl: rmtdevImg,
+    imageUrl: netflixroulettepic,
   },
   {
     title: "Particle",
-    description: "Im and the sole developer for the Particle website. I work with the marketing team to make sure they are able to interact with their customers through this website. We use both Contentful CMS and a headless WordPress to allow the marketing team to manage the content on the site.",
+    url: "https://particle.io",
+    description: "Im the sole developer for this Particle website. I work with the marketing team to make sure they are able to interact with their customers through this website. We use both Contentful CMS and a headless WordPress to allow the marketing team to manage the content on the site.",
     tags: ["Gatsby.js", "GraphQL", "Styled Components", "Redux", "Contentful CMS", "Headless WordPress", "Netlify", "Intersection Observer", "Segment", "Marketo API", "GreenHouse Job Board API" ],
-    imageUrl: wordanalyticsImg,
+    imageUrl: particlepic,
+  },
+  {
+    title: "Particle store",
+    url: "https://store.particle.io",
+    description: "Im the sole developer for this Shopify store. I worked with the design team to create a custom Shopify theme thats allows them to showcase their brand, message, and product line. I also built a custom Shopify app for this store to add extra functionality where Shopify has limitations.",
+    tags: ["Shopify", "Liquid", "Javascript", "Sass", "Shopify App Development", "Shopify Storefront API", "Shopify Admin API", "Shopify Webhooks", "Shopify Embedded App SDK", "Shopify CLI"],
+    imageUrl: storepic,
   },
 ] as const;
 
